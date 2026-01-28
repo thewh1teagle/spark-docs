ee3cc2610549d2ceddce0c3a277eaf307442c988
yakov <61390950+thewh1teagle@users.noreply.github.com>
2026-01-28 04:56:21 +0200

    simple docs

--- use-k2/main.py
+++
@@ -1,7 +0,0 @@
-import k2
-
-version = k2.__dev_version__
-print(f"K2 version: {version}")
-
-
-
--- use-onnxruntime/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from use-onnxruntime!")
-
-
-if __name__ == "__main__":
-    main()
